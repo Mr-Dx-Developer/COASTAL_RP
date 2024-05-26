f9c7f4debb9b451f7c9416120a43bd399c3096e1
Tanvir Anjum Neon <taneon98@gmail.com>
2024-05-26 21:57:27 +0600

    GITHUB SETUP @everyone

---
+++ [qb]/qb-doorlock/configs/Prison Doors.lua
@@ -0,0 +1,239 @@
+
+
+-- prison_door_sidewalk_01 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_sidewalk_01'] = {
+    doorRate = 1.0,
+    distance = 2,
+    objName = -1156020871,
+    objCoords = vec3(1797.760864, 2596.564941, 46.387306),
+    doorType = 'door',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 179.99987792969,
+    doorLabel = 'Stilldoor',
+    locked = true,
+}
+
+-- prison_door_mainwalk_01 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_mainwalk_01'] = {
+    doorRate = 1.0,
+    distance = 2,
+    objName = -684929024,
+    objCoords = vec3(1831.341431, 2594.993408, 46.037949),
+    doorType = 'door',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 89.999961853027,
+    doorLabel = 'maindoor',
+    locked = true,
+}
+
+-- prison_door_mainwalk_02 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_mainwalk_02'] = {
+    doorRate = 1.0,
+    distance = 2,
+    objName = -684929024,
+    objCoords = vec3(1837.743408, 2592.163086, 46.039612),
+    doorType = 'door',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 0.0,
+    doorLabel = 'maindoor2',
+    locked = true,
+}
+
+-- prison_door_staffwalk_01 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_staffwalk_01'] = {
+    doorRate = 1.0,
+    distance = 1,
+    objName = 2024969025,
+    objCoords = vec3(1837.635254, 2576.992920, 46.038635),
+    doorType = 'door',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 0.0,
+    doorLabel = 'staffdoor1',
+    locked = true,
+}
+
+-- prison_door_staffwalk_02 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_staffwalk_02'] = {
+    doorRate = 1.0,
+    distance = 1,
+    objName = 2024969025,
+    objCoords = vec3(1844.405273, 2576.998291, 46.035641),
+    doorType = 'door',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 0.0,
+    doorLabel = 'staffdoor2',
+    locked = true,
+}
+
+-- prison_door_vehientry_01 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_01'] = {
+    doorRate = 1.0,
+    distance = 10,
+    objName = 741314661,
+    objCoords = vec3(1845.28, 2608.477, 45.59),
+    doorType = 'sliding',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 90.0,
+    doorLabel = 'Vehidoor1',
+    locked = true,
+}
+
+-- prison_door_vehientry_02 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_02'] = {
+    doorRate = 1.0,
+    distance = 10,
+    objName = 741314661,
+    objCoords = vec3(1818.844, 2608.25, 45.592),
+    doorType = 'sliding',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 90.0,
+    doorLabel = 'Vehidoor2',
+    locked = true,
+}
+
+-- prison_door_vehientry_03 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_03'] = {
+    doorRate = 1.0,
+    distance = 10,
+    objName = 741314661,
+    objCoords = vec3(1801.564, 2617.029, 45.622),
+    doorType = 'sliding',
+    fixText = false,
+    authorizedJobs = { ['police'] = 0 },
+    objYaw = 179.99998474121,
+    doorLabel = 'Vehidoor3',
+    locked = true,
+}
+
+-- prison_door_vehientry_04 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_04'] = {
+    doors = {
+        {objName = 741314661, objYaw = 289.16897583008, objCoords = vec3(1831.705, 2700.191, 45.428)},
+        {objName = 741314661, objYaw = 110.00004577637, objCoords = vec3(1834.262, 2692.713, 45.43)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor4',
+    distance = 10,
+}
+
+-- prison_door_vehientry_05 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_05'] = {
+    doors = {
+        {objName = 741314661, objYaw = 339.62002563477, objCoords = vec3(1765.907, 2751.398, 45.427)},
+        {objName = 741314661, objYaw = 160.00001525879, objCoords = vec3(1773.366, 2748.664, 45.427)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor5',
+    distance = 8,
+}
+
+-- prison_door_vehientry_06 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_06'] = {
+    doors = {
+        {objName = 741314661, objYaw = 27.17546081543, objCoords = vec3(1651.469, 2743.586, 45.44)},
+        {objName = 741314661, objYaw = 207.17547607422, objCoords = vec3(1658.575, 2747.256, 45.44)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor6',
+    distance = 8,
+}
+
+-- prison_door_vehientry_07 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_07'] = {
+    doors = {
+        {objName = 741314661, objYaw = 54.548603057861, objCoords = vec3(1577.745, 2670.463, 45.475)},
+        {objName = 741314661, objYaw = 233.70986938477, objCoords = vec3(1582.038, 2676.653, 45.471)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor7',
+    distance = 8,
+}
+
+-- prison_door_vehientry_08 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_08'] = {
+    doors = {
+        {objName = 741314661, objYaw = 87.0146484375, objCoords = vec3(1546.905, 2579.9, 45.389)},
+        {objName = 741314661, objYaw = 267.01473999023, objCoords = vec3(1547.221, 2587.543, 45.389)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor8',
+    distance = 8,
+}
+
+-- prison_door_vehientry_09 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_09'] = {
+    doors = {
+        {objName = 741314661, objYaw = 118.04624938965, objCoords = vec3(1556.089, 2472.675, 45.387)},
+        {objName = 741314661, objYaw = 298.04623413086, objCoords = vec3(1552.582, 2479.061, 45.387)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor9',
+    distance = 8,
+}
+
+-- prison_door_vehientry_10 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_10'] = {
+    doors = {
+        {objName = 741314661, objYaw = 173.00039672852, objCoords = vec3(1663.944, 2407.826, 45.401)},
+        {objName = 741314661, objYaw = 353.00042724609, objCoords = vec3(1656.926, 2408.812, 45.401)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor10',
+    distance = 8,
+}
+
+-- prison_door_vehientry_11 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_11'] = {
+    doors = {
+        {objName = 741314661, objYaw = 206.12844848633, objCoords = vec3(1759.111, 2424.521, 45.421)},
+        {objName = 741314661, objYaw = 26.757732391357, objCoords = vec3(1752.669, 2421.285, 45.421)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor11',
+    distance = 8,
+}
+
+-- prison_door_vehientry_12 created by Briggzy
+Config.DoorList['Prison Doors-prison_door_vehientry_12'] = {
+    doors = {
+        {objName = 741314661, objYaw = 251.97775268555, objCoords = vec3(1812.75, 2484.963, 45.445)},
+        {objName = 741314661, objYaw = 70.905723571777, objCoords = vec3(1810.532, 2478.15, 45.445)}
+    },
+    doorType = 'doublesliding',
+    doorRate = 1.0,
+    authorizedJobs = { ['police'] = 0 },
+    locked = true,
+    doorLabel = 'Vehidoor12',
+    distance = 8,
+}
