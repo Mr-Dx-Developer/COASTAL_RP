3e9946cba039a1f7df85b96ceb4df6eb7fa1c4a0
Tanvir Anjum Neon <taneon98@gmail.com>
2024-06-20 03:23:49 +0600

    update wasibi

--- [dependency]/wasabi_bridge/inventories/qb-inventory/client.lua
+++ [dependency]/wasabi_bridge/inventories/qb-inventory/client.lua
@@ -11,7 +11,17 @@ if qsFound == 'started' or qsFound == 'starting' then return end
 WSB.inventorySystem = 'qb-inventory'
 WSB.inventory = {}
 
+OldInventory = GetResourceMetadata(WSB.inventorySystem, 'version', 0)
+OldInventory = OldInventory:gsub('%.', '')
+OldInventory = tonumber(OldInventory)
+if not OldInventory or OldInventory >= 200 then OldInventory = false end
+
 function WSB.inventory.openPlayerInventory(targetId)
+    if not OldInventory then
+        TriggerServerEvent('wasabi_bridge:openPlayerInventory', targetId)
+        return
+    end
+
     TriggerServerEvent('inventory:server:OpenInventory', 'otherplayer', targetId)
     TriggerEvent('inventory:server:RobPlayer', targetId)
 end
@@ -20,6 +30,11 @@ function WSB.inventory.openStash(data)
     -- data = {name = name, unique = true, maxWeight = maxWeight, slots = slots}
     if data.unique then
         data.name = ('%s_%s'):format(data.name, WSB.getIdentifier())
+    end
+
+    if not OldInventory then
+        TriggerServerEvent('wasabi_bridge:openStash', data)
+        return
     end
 
     TriggerServerEvent('inventory:server:OpenInventory', 'stash', data.name)
@@ -31,14 +46,18 @@ end
 
 function WSB.inventory.openShop(data)
     --[[
-data = {
-    identifier = 'shop_identifier',
-    name = 'Shop Name',
-    inventory = {
-        { name = 'item_name', price = 100 },
-    }
-]]
-    local shopData = ConvertShopData(data)
+    data = {
+        identifier = 'shop_identifier',
+        name = 'Shop Name',
+        inventory = {
+            { name = 'item_name', price = 100 },
+        }
+    ]]
+    if not OldInventory then
+        TriggerServerEvent('wasabi_bridge:registerShop', data)
+        return
+    end
 
+    local shopData = ConvertShopData(data)
     TriggerServerEvent("inventory:server:OpenInventory", "shop", data.identifier, shopData)
 end
--- [dependency]/wasabi_bridge/inventories/qb-inventory/server.lua
+++ [dependency]/wasabi_bridge/inventories/qb-inventory/server.lua
@@ -11,6 +11,43 @@ if qsFound == 'started' or qsFound == 'starting' then return end
 WSB.inventory = {}
 WSB.inventorySystem = 'qb-inventory'
 
+OldInventory = GetResourceMetadata(WSB.inventorySystem, 'version', 0)
+OldInventory = OldInventory:gsub('%.', '')
+OldInventory = tonumber(OldInventory)
+if not OldInventory or OldInventory >= 200 then OldInventory = false end
+
+if not OldInventory then
+    local registeredShops = {}
+    RegisterNetEvent('wasabi_bridge:registerShop', function(data)
+        local src = source
+        if registeredShops[data.identifier] then
+            exports['qb-inventory']:OpenShop(src, data.identifier)
+            return
+        end
+
+        exports['qb-inventory']:CreateShop({
+            name = data.identifier,
+            label = data.name,
+            slots = #data.inventory,
+            items = data.inventory -- { name = 'sandwich', price = 5 }
+        })
+        registeredShops[data.identifier] = data
+        exports['qb-inventory']:OpenShop(src, data.identifier)
+    end)
+
+    RegisterNetEvent('wasabi_bridge:openStash', function(data)
+        local src = source
+        exports['qb-inventory']:OpenInventory(src, data.name,
+            { label = data.name, slots = data.slots, maxweight = data.maxWeight })
+    end)
+
+    RegisterNetEvent('wasabi_bridge:openPlayerInventory', function(targetId)
+        local src = source
+        exports['qb-inventory']:OpenInventoryById(src, targetId)
+    end)
+end
+
+
 function WSB.inventory.getItemSlot(source, itemName)
     return GetItemSlot(source, itemName) or false
 end
