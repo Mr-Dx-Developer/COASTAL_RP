9641f667408ab888f5ac644ed0e77c0580c87cae
Tanvir Anjum Neon <taneon98@gmail.com>
2024-06-23 08:58:07 +0600

    OLD CHAT REMOVE

--- [dependency]/2na_chat/Client/nui.lua
+++
@@ -1,7 +0,0 @@
-RegisterNuiCallback("hide", function() 
-    TriggerEvent("chat:hide")
-end)
-
-RegisterNuiCallback("processInput", function(data)
-    ExecuteCommand(data.command) 
-end)
--- [dependency]/2na_core/Config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.MySQL = "oxmysql" -- Supports both mysql-async and oxmysql
-
-Config.Framework = "QB" -- Supports both ESX and QB
