fbe4468b9ec0f6c7a52c209e9e89a1841b5e6f19
Tanvir Anjum Neon <taneon98@gmail.com>
2024-06-25 21:15:11 +0600

    POLICE IMPOUND

--- [qb]/cd_garage/configs/config.lua
+++ [qb]/cd_garage/configs/config.lua
@@ -845,11 +845,11 @@ Config.ImpoundLocations = { --DO NOT CHANGE THE TABLE IDENTIFIERSs, for example 
 
     ['car_2'] = { 
         ImpoundID = 2,
-        coords = {x = 1893.48, y = 3713.50, z = 32.77},
-        spawnpoint = {x = 1887.123, y = 3710.348, z = 31.92, h = 212.0},
+        coords = {x = 470.97, y = -998.601, z = 25.141},
+        spawnpoint = {x = 470.97, y = -998.601, z = 25.141, h = 85.253},
         blip = {
             sprite = 357,
-            scale = 0.5,
+            scale = 0.1,
             colour = 3,
             name = L('car_sandy_impound'),
         }
