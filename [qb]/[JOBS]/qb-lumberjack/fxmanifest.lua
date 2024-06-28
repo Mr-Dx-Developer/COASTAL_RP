23a36b72f9764685ac57993e3dd38e43eb2ae88d
Tanvir Anjum Neon <taneon98@gmail.com>
2024-06-29 01:25:29 +0600

    Wood Job Add

---
+++ [qb]/[JOBS]/qb-lumberjack/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KT_BRAVEHEART#9005'
+description 'Simple lumberjack job for your FiveM Server By Braveheart'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+}
+
+server_script 'server/*.lua'
