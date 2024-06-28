e2c2353389e3f3a11253d2e398917cf99f842392
Tanvir Anjum Neon <taneon98@gmail.com>
2024-06-29 01:36:09 +0600

    Weather modefie

--- [qb]/qb-weathersync/fxmanifest.lua
+++ [qb]/qb-weathersync/fxmanifest.lua
@@ -1,28 +1,16 @@
-shared_script '@BRPS-GUARD/shared_fg-obfuscated.lua'
-shared_script '@BRPS-GUARD/ai_module_fg-obfuscated.lua'
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-use_fxv2_oal 'yes'
+author 'Kakarot'
+description 'Syncs the time & weather for all players on the server and allows editing by command'
+version '2.1.0'
 
-name 'Renewed Weather Sync'
-author 'FjamZoo - Renewed Scripts'
-version '1.1.3'
-
-shared_scripts{
-    '@ox_lib/init.lua',
+shared_scripts {
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua'
 }
 
-client_scripts {
-    'client/*.lua',
-    'compatability/**/client.lua',
-}
-
-server_scripts {
-    'server/time.lua',
-    'server/weather.lua',
-    'compatability/**/server.lua',
-}
-
-provide 'qb-weathersync'
-provide 'cd_easytime'
+server_script 'server/server.lua'
+client_script 'client/client.lua'
