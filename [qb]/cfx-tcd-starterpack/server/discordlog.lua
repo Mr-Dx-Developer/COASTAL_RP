f83fa8056ba7e52ae6caf29b50b7bec815cfe1f5
Tanvir Anjum Neon <taneon98@gmail.com>
2024-06-29 01:31:15 +0600

    Welcome Pack Auto System

---
+++ [qb]/cfx-tcd-starterpack/server/discordlog.lua
@@ -0,0 +1,6 @@
+DiscordConfig = {
+    ["webhook"] = "https://discord.com/api/webhooks/1256374457698877602/TdT0VvR6aUj3Z5AUn8VbLC5L1F1hHQyNPxuxBULhgtcRMrQ3YaU3AUXFRJPkfdSllx0y", -- Your webhook here
+    ["title"] = "Starterpack Logs",
+    ["thumbnail"] = "https://i.imgur.com/6s82WUZ.png",
+    ["color"] = "3447003" -- HEX
+}
