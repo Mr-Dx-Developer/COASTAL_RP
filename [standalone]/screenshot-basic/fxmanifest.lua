891496eb883895573956516e758f4e43003ff901
Tanvir Anjum Neon <taneon98@gmail.com>
2024-06-29 01:38:41 +0600

    police siren

--- [standalone]/screenshot-basic/fxmanifest.lua
+++ [standalone]/screenshot-basic/fxmanifest.lua
@@ -1,19 +1,13 @@
-shared_script '@BRPS-GUARD/ai_module_fg-obfuscated.js'
 fx_version 'bodacious'
 game 'common'
-
 client_script 'dist/client.js'
 server_script 'dist/server.js'
-
 dependency 'yarn'
 dependency 'webpack'
-
 webpack_config 'client.config.js'
 webpack_config 'server.config.js'
 webpack_config 'ui.config.js'
-
 files {
     'dist/ui.html'
 }
-
 ui_page 'dist/ui.html'
