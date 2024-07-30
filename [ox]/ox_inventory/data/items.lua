123f036fb03a0b0fba94adb44c9f9fe6ca3b5e1e
Tanvir Anjum Neon <taneon98@gmail.com>
2024-07-30 14:02:35 +0600

    inventory

--- [ox]/ox_inventory/data/items.lua
+++ [ox]/ox_inventory/data/items.lua
@@ -680,6 +680,37 @@ return {
 		}
 	},
 
+	["stethascope"] = {
+		label = "Stetha Scope",
+		weight = 100,
+		stack = true,
+		close = true,
+		description = "!",
+		client = {
+			image = "stethascope.png",
+		}
+	},
+	["specialwatch"] = {
+		label = "Special Watch",
+		weight = 100,
+		stack = true,
+		close = true,
+		description = "!",
+		client = {
+			image = "specialwatch.png",
+		}
+	},
+	["blueusb"] = {
+		label = "Blue usb Device",
+		weight = 100,
+		stack = true,
+		close = true,
+		description = "!",
+		client = {
+			image = "blueusb.png",
+		}
+	},
+
 	["rolex"] = {
 		label = "Golden Watch",
 		weight = 1000,
@@ -1442,6 +1473,76 @@ return {
 		}
 	},
 
+	["art1"] = {
+        label = "Kitty Sleeping Art",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "This Is Too Cute",
+        client = {
+            image = "art1.png",
+        }
+    },
+	["art2"] = {
+        label = "Wide Eye Kitty Art",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "This Is Too Cute",
+        client = {
+            image = "art2.png",
+        }
+    },
+	["art3"] = {
+        label = "Fancy Kitty Art",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "This Is Too Cute",
+        client = {
+            image = "art3.png",
+        }
+    },
+	["art4"] = {
+        label = "Presidential Kitty Art",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "Id Vote For This",
+        client = {
+            image = "art4.png",
+        }
+    },
+	["art5"] = {
+        label = "Obi Jesus Painting",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "I Swore My Allegiance To The Force, To Heaven!",
+        client = {
+            image = "art5.png",
+        }
+    },
+	["art6"] = {
+        label = "Merp Kitty Art",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "Merp",
+        client = {
+            image = "art6.png",
+        }
+    },
+	["art7"] = {
+        label = "Family Portait",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "Smile",
+        client = {
+            image = "art7.png",
+        }
+    },
 	["electronickit"] = {
 		label = "Raspberry Pi",
 		weight = 100,
@@ -2224,6 +2325,67 @@ return {
 		weight = 45,
 		stack = true
 	},
+
+	["checkbook"] = {
+        label = "Check Book",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "Do People Use These?",
+        client = {
+            image = "checkbook.png",
+        }
+    },
+	["mdlaptop"] = {
+        label = "Slow Laptop",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "Can I Download More Ram?",
+        client = {
+            image = "laptop.png",
+        }
+    },
+	["mddesktop"] = {
+        label = "Desktop",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "I hope there isnt a virus",
+        client = {
+            image = "mddesktop.png",
+        }
+    },
+	["mdmonitor"] = {
+        label = "Monitor",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "720HD bb",
+        client = {
+            image = "mdmonitor.png",
+        }
+    },
+	["mdtablet"] = {
+        label = "Tablet",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "Never Will Give This Up",
+        client = {
+            image = "mdtablet.png",
+        }
+    },
+	["mdspeakers"] = {
+        label = "Speakers",
+        weight = 2500,
+        stack = false,
+        close = false,
+        description = "Is This Even Loud?",
+        client = {
+            image = "speaker.png",
+        }
+    },
 
 	['dj_deck'] = {
 		label = 'DJ Deck',
