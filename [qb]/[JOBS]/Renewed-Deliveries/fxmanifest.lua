0a3663c4d0335459bda8aea421ad968d9905b8fb
Tanvir Anjum Neon <taneon98@gmail.com>
2024-07-30 12:51:53 +0600

    DELIVERY ADD

--- [qb]/[JOBS]/Renewed-Deliveries/fxmanifest.lua
+++ [qb]/[JOBS]/Renewed-Deliveries/fxmanifest.lua
@@ -7,7 +7,7 @@ version '1.0.0'
 lua54 'yes' -- Add in case you want to use lua 5.4 (https://www.lua.org/manual/5.4/manual.html)
 
 shared_script {
-    "@qb-shops/config.lua",
+ --   "@qb-shops/config.lua",
     'shared/*.lua'
 }
 
