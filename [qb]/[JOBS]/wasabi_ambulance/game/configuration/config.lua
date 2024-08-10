e81ba37ac745d4aba90714eb5f980960c7217ad2
Tanvir Anjum Neon <taneon98@gmail.com>
2024-08-10 12:43:44 +0600

    Respawn Timer

--- [qb]/[JOBS]/wasabi_ambulance/game/configuration/config.lua
+++ [qb]/[JOBS]/wasabi_ambulance/game/configuration/config.lua
@@ -289,7 +289,7 @@ Config.jobMenu = 'F6'               -- Default job menu key
 Config.billingSystem = false        -- Current options: 'esx' (For esx_billing) / 'qb' (For qbcore users) 'okok' (For okokBilling) / 'pefcl' (For NPWD billing system) (Easy to add more in editable client - SET TO false IF UNDESIRED) or of course false to disable
 Config.targetSystem = true          -- Target system for targetting players, medbags, and stretcher(If disabled with replace with menus/3D text) (Compatible out of the box with qTarget, qb-target, and ox_target)
 
-Config.RespawnTimer = 5 * minutes   -- Time before optional respawn
+Config.RespawnTimer = 15 * minutes   -- Time before optional respawn
 Config.BleedoutTimer = 45 * minutes -- Time before it forces respawn
 Config.ChargeForRevive = {
     enabled = true,                -- Charge players to revive after the timer expires when they hold E to revive?
