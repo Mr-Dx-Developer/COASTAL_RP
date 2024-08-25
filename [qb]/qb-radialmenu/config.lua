2c2d6924b49121275bbd09e37d57b911f21f7ab9
Tanvir Anjum Neon <taneon98@gmail.com>
2024-08-25 20:47:39 +0600

    foor lock

--- [qb]/qb-radialmenu/config.lua
+++ [qb]/qb-radialmenu/config.lua
@@ -11,14 +11,15 @@ Config.MenuItems = {
         title = 'Citizen',
         icon = 'user',
         items = {
-            {
+            --[[ {
                 id = 'givenum',
                 title = 'Give Contact Details',
                 icon = 'address-book',
                 type = 'client',
                 event = 'lb-phone:client:GiveContactDetails',
                 shouldClose = true
-            }, {
+            }, ]] 
+            {
             id = 'getintrunk',
             title = 'Get In Trunk',
             icon = 'car',
